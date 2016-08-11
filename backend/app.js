9eec26d683d91274b8f611268e191f11d889761d
Yassin Alkhalili <yaseenit@gmail.com>
2016-08-12 01:18:12 +0200

    user model

--- backend/app.js
+++ backend/app.js
@@ -1,60 +1,58 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var express = require('express'),
+    mongoose = require('mongoose'),
+    morgan = require('morgan'),
+    bodyParser = require('body-parser'),
+   // passport = require('./config/passport'),
+    session = require('express-session');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+
+
+var db;
+console.log('Hello');
+if(process.env.ENV == 'Test'){
+
+    db = mongoose.connect('mongodb://localhost/cmsAPI_test');
+}
+
+else{
+    db= mongoose.connect('mongodb://localhost/cmsAPI');
+}
+
+var User = require('./models/userModel');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+//var passport = passport(User);
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+var port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(morgan('dev'));
 
-app.use('/', routes);
-app.use('/users', users);
+userRouter = require('./Routes/userRoutes')(User);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+
+app.use('/api/users', userRouter); 
+
+
+app.get('/', function(req, res){
+    res.send('welcome to my API!');
 });
+//////////Session/////////
+app.use(session({
+       saveUninitialized: true,
+       resave: true,
+       secret: "myBigSecret:)"
+}));
+//////////passport////////////
+//var passport = require('passport');
+//app.use(passport.initialize());
+//app.use(passport.session());
 
-// error handlers
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+app.listen(port, function(){
+    console.log('Gulp is running my app on  PORT: ' + port);
 });
-
 
 module.exports = app;
