aa3171de585fe8a9cb30e54cc89abc06d6b443bf
Yassin Alkhalili <yaseenit@gmail.com>
2016-08-15 22:41:54 +0200

    starting RESTful structure

--- backend/gulpfile.js
+++ backend/gulpfile.js
@@ -7,7 +7,7 @@ var gulp = require('gulp'),
 
 gulp.task('default', function(){
     nodemon({
-        script: 'app.js',
+        script: 'server.js',
         ext: 'js',
         env: {
             PORT:8000
