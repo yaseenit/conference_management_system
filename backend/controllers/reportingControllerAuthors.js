414da8b216b2ce5d96791b3c80da57640a5794d9
Ahmed Mamdouh <ahmed.mamdouh@brightskiesinc.com>
2016-09-07 18:13:23 +0200

    test group by authors and confTitle in each conference

--- backend/controllers/reportingControllerAuthors.js
+++ backend/controllers/reportingControllerAuthors.js
@@ -3,11 +3,11 @@ var reportingControllerAuthors = function () {
 
     var get = function (req, res) {
       var agg= [
-          { $match: { /* Query can go here, if you want to filter results. */ } } 
-  , { $project: { authors: 1 } } /* select the tokens field as something we want to "send" to the next command in the chain */
+         { $match: { /* Query can go here, if you want to filter results. */ } } ,
+   { $project: { authors: 1 } } 
   , { $unwind: '$authors' } /* this converts arrays into unique documents for counting */
   , { $group: { /* execute 'grouping' */
-          _id: { authors: '$authors' } /* using the 'token' value as the _id */
+          _id: { authors: '$authors',conftitle:'$title' } /* using the 'token' value as the _id */
         , count: { $sum: 1 } /* create a sum value */
       }
     }
